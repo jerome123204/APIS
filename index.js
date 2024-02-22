7034304733c2ccabf9e15e84570fdd202c4b1172
wedfhujkkmhhgg233 <154974042+wedfhujkkmhhgg233@users.noreply.github.com>
2024-02-23 07:01:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const chalk = require("chalk");
 const { execSync } = require("child_process");
 
 const app = express();
-const port = 2000;
+const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
